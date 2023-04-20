4c32e9dad1618c3851d113694aeb3b98d9e5754c
SeyyedKhandon <seyyedkhandon@gmail.com>
2023-04-20 16:41:34 +0330

    build: :art: Add sugar path via @

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Button from "@/components/button";
+
 function App() {
-  return <div className="text-8xl">SolActive </div>;
+  return (
+    <div className="text-8xl">
+      <Button />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/button/index.tsx
@@ -0,0 +1,5 @@
+export default function Button() {
+  return (
+    <button className="bg-slate-900 px-2 py-4 text-slate-200">Click Me</button>
+  );
+}
